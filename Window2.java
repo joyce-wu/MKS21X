7b9e2e33b0e7ee69f33091fdb43f53a14b8b4b3a
Joyce Wu <jwu26@stuy.edu>
2016-12-21 22:38:39 -0500

    working temp gui

--- Window2.java
+++ Window2.java
@@ -1,35 +1,66 @@
 import javax.swing.*;
 import java.awt.*;
-public class Window2 extends JFrame {
+import java.awt.event.*;
+
+public class Window2 extends JFrame implements ActionListener {
     private Container pane;
 
-    private JButton b, a;
-    private JLabel l, f;
-    private JTextField t, r;
+    private JButton b, b2;
+    private JLabel l, l2;
+    private JTextField f, c;
  
     //CONSTRUCTOR SETS EVERYTHING UP
     public Window2() {
 	this.setTitle("Temperature Conversion");
-	this.setSize(600,400);
+	this.setSize(200,200);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     
 	pane = this.getContentPane();
-	pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS)); 
-    
+	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS)); 
 	b = new JButton("FtoC");
-	a = new JButton("CtoF");
-	l = new JLabel("Fahrenheit",null,JLabel.CENTER);
-	f = new JLabel("Celsius", null, JLabel.CENTER);
-	t = new JTextField(12);
-	r = new JTextField(12);
+	b.addActionListener(this);
+	b.setActionCommand("FtoC");
+	b2 = new JButton("CtoF");
+	b2.addActionListener(this);
+	b2.setActionCommand("CtoF");
+	l = new JLabel("Fahrenheit");
+	l2 = new JLabel("Celsius");
+	f = new JTextField(1);
+	c = new JTextField(1);
 	pane.add(l);
 	pane.add(f);
-	pane.add(t);
-	pane.add(r);
 	pane.add(b);
-	pane.add(a);
+	pane.add(l2);
+	pane.add(c);
+	pane.add(b2);
     }
+
+    public void actionPerformed(ActionEvent e){
+	String event = e.getActionCommand();
+	if(event.equals("FtoC")){
+	    String s = f.getText();
+	    try{
+	    	int num = Integer.parseInt(s);
+	    	f.setText(""+num);
+	    	c.setText(Temperatures.FtoC(num)+"");
+	    }catch(NumberFormatException a){
+	    	c.setText("ERROR");
+	    	f.setText("ERROR");
+	    }
+	}
+	else if(event.equals("CtoF")){
+	    String s = c.getText();
+	    try{
+	    	int num = Integer.parseInt(s);
+	    	f.setText(Temperatures.CtoF(num)+"");
+	    }catch(NumberFormatException a){
+	    	c.setText("ERROR");
+	    	f.setText("ERROR");
+	    }
+	}
+    }
+	
 
     //MAIN JUST INSTANTIATES + MAKE VISIBLE
     public static void main(String[] args) {
